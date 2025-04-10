0c98b6281652e5e12bb94cd6e7910fe0c4990200
Danish Ali <dali27363@gmail.com>
2025-04-10 16:47:07 +0500

    complete hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import BgGradient from "@/components/common/bg-gradient";
+import CTASection from "@/components/home/cta-section";
+import DemoSection from "@/components/home/demo-section";
+import HeroSection from "@/components/home/hero-section";
+import HowItWorks from "@/components/home/how-it-works";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Shadcn Button </Button>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+
+      <DemoSection />
+
+      {/* <HowItWorks /> */}
+
+      {/* <PricingSection /> */}
+
+      {/* <CTASection /> */}
     </div>
   );
 }
