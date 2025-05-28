4f256e39d3a544bd0dd56bec404f679db68e226e
Danish Ali <dali27363@gmail.com>
2025-05-28 22:20:14 +0500

    feat: update ProgressBar component to enhance layout and functionality with section mapping

--- src/components/dashboard/summaries/progress-bar.tsx
+++ src/components/dashboard/summaries/progress-bar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const ProgressBar = ({
   sections,
@@ -7,7 +7,26 @@ const ProgressBar = ({
   sections: any[];
   currentSection: number;
 }) => {
-  return <div className='absolute top-0 left-0 right-0 z-20 bg-background/80 backdrop-blur-xs pt-4'>Progress Bar</div>;
+  return (
+    <div className="absolute top-0 left-0 right-0 z-20 bg-background/80 backdrop-blur-xs pt-4 pb-2 border-b border-rose-50/10">
+      <div className="px-4 flex gap-1 5">
+        {sections.map((_, index) => (
+          <div className="h-1.5 flex-1 rounded-full bg-rose-500/10 overflow-hidden">
+            <div
+              key={index}
+              className={`h-1.5 bg-linear-to-r from-gray-500 to-rose-600 transition-all duration-300 ${
+                index === currentSection
+                  ? "w-full"
+                  : currentSection > index
+                  ? "w-full opacity-10"
+                  : "w-0"
+              }`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default ProgressBar
+export default ProgressBar;
